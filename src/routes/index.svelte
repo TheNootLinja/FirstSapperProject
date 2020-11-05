65c5ef83a5d4fd0d94b454b7abd94344728f2b2c
TheNootLinja <thenootlinja@gmail.com>
2020-11-04 20:59:25 -0500

    Finish Sapper app and run export and build commands to get bundles for deployment

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,52 +4,53 @@
   export function preload(page) {
     // Need to remember that in the module context script we have to use this.fetch
     // instead of just fetch.
-    return this.fetch("https://svelte-course-e7a24.firebaseio.com/meetups.json")
-    // In the first then block we are checking if the response is
-    // in the 200 range and if not throwing a new error.
-    .then((res) => {
-      if (!res.ok) {
-        throw new Error("An error has occured, please try again.");
-      }
-      // If we make it past the res.ok check then we return the
-      // response parsed as json
-      return res.json();
-    })
-    // After returning the response we have our data in a
-    // rough form.
-    .then((data) => {
-      const loadedMeetups = [];
-      for (const key in data) {
-        loadedMeetups.push({
-          ...data[key],
-          id: key,
-        });
-      }
-      return { fetchedMeetups: loadedMeetups.reverse() };
-    })
-    .catch((err) => {
-      console.log(err);
-      isLoading = false;
-      error = err;
-      this.error(500, 'Could not fetch meetups!');
-    });
-  };
+    return (
+      this.fetch('https://svelte-course-e7a24.firebaseio.com/meetups.json')
+        // In the first then block we are checking if the response is
+        // in the 200 range and if not throwing a new error.
+        .then((res) => {
+          if (!res.ok) {
+            throw new Error('An error has occured, please try again.');
+          }
+          // If we make it past the res.ok check then we return the
+          // response parsed as json
+          return res.json();
+        })
+        // After returning the response we have our data in a
+        // rough form.
+        .then((data) => {
+          const fetchedMeetups = [];
+          for (const key in data) {
+            fetchedMeetups.push({
+              ...data[key],
+              id: key,
+            });
+          }
+          return { fetchedMeetups: fetchedMeetups.reverse() };
+        })
+        .catch((err) => {
+          console.log(err);
+          isLoading = false;
+          error = err;
+          this.error(500, 'Could not fetch meetups!');
+        })
+    );
+  }
 </script>
 
 <script>
-  import { fade, fly, slide, scale } from "svelte/transition";
-  import { flip } from "svelte/animate";
-  import { createEventDispatcher, onMount, onDestroy } from "svelte";
-  import meetups from "../meetups-store"
-  import MeetupItem from "../components/Meetup/MeetupItem.svelte";
-  import MeetupFilter from "../components/Meetup/MeetupFilter.svelte";
-  import Button from "../components/UI/Button.svelte";
-  import EditMeetup from "../components/Meetup/EditMeetup.svelte"
-  import LoadingSpinner from "../components/UI/LoadingSpinner.svelte"
+  import { fade, fly, slide, scale } from 'svelte/transition';
+  import { flip } from 'svelte/animate';
+  import { createEventDispatcher, onMount, onDestroy } from 'svelte';
+  import meetups from '../meetups-store';
+  import MeetupItem from '../components/Meetup/MeetupItem.svelte';
+  import MeetupFilter from '../components/Meetup/MeetupFilter.svelte';
+  import Button from '../components/UI/Button.svelte';
+  import EditMeetup from '../components/Meetup/EditMeetup.svelte';
+  import LoadingSpinner from '../components/UI/LoadingSpinner.svelte';
 
   export let fetchedMeetups;
 
-  let loadedMeetups = [];
   let editMode;
   let editedId;
   let isLoading;
@@ -59,13 +60,15 @@
 
   let favsOnly = false;
 
-  $: filteredMeetups = favsOnly ? loadedMeetups.filter((m) => m.isFavorite) : loadedMeetups;
+  $: filteredMeetups = favsOnly
+    ? fetchedMeetups.filter((m) => m.isFavorite)
+    : fetchedMeetups;
 
   onMount(() => {
-    unsubscribe = meetups.subscribe(items => {
-      loadedMeetups = items;
-    })
     meetups.setMeetups(fetchedMeetups);
+    unsubscribe = meetups.subscribe((items) => {
+      fetchedMeetups = items;
+    });
   });
 
   onDestroy(() => {
@@ -89,12 +92,12 @@
   }
 
   function startEdit(event) {
-    editMode = "edit";
+    editMode = 'edit';
     editedId = event.detail;
   }
 
   function startAdd() {
-    editMode = "edit";
+    editMode = 'edit';
   }
 </script>
 
@@ -133,29 +136,27 @@
 {#if isLoading}
   <LoadingSpinner />
 {:else}
-<section id="meetup-controls">
-  <MeetupFilter on:select={filterMeetup} />
-  <Button on:click={startAdd}>New Meetup</Button>
-</section>
-{#if filteredMeetups.length === 0}
-  <p id="no-meetups">No meetups found. Start adding some!</p>
+  <section id="meetup-controls">
+    <MeetupFilter on:select={filterMeetup} />
+    <Button on:click={startAdd}>New Meetup</Button>
+  </section>
+  {#if filteredMeetups.length === 0}
+    <p id="no-meetups">No meetups found. Start adding some!</p>
+  {/if}
+  <section id="meetups">
+    {#each filteredMeetups as meetup (meetup.id)}
+      <div transition:scale animate:flip={{ duration: 450 }}>
+        <MeetupItem
+          id={meetup.id}
+          title={meetup.title}
+          subtitle={meetup.subtitle}
+          description={meetup.description}
+          imageUrl={meetup.image}
+          email={meetup.contactEmail}
+          address={meetup.address}
+          isFav={meetup.isFavorite}
+          on:edit={startEdit} />
+      </div>
+    {/each}
+  </section>
 {/if}
-<section id="meetups">
-  {#each filteredMeetups as meetup (meetup.id)}
-    <div transition:scale animate:flip={{ duration: 450 }}>
-      <MeetupItem
-        id={meetup.id}
-        title={meetup.title}
-        subtitle={meetup.subtitle}
-        description={meetup.description}
-        imageUrl={meetup.image}
-        email={meetup.contactEmail}
-        address={meetup.address}
-        isFav={meetup.isFavorite}
-        on:edit = {startEdit} />
-    </div>
-  {/each}
-</section>
-{/if}
-
-
